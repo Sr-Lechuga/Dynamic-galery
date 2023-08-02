f6565cff30ad33cc46874dcffca75540584fcd1e
Sr.Lechuga <jonattanlima@gmail.com>
2023-08-02 00:20:41 -0300

    Dynamic Galery

--- code/script.js
+++ code/script.js
@@ -1,1 +1,18 @@
 "use strict";
+
+const archivo = document.getElementById('fileSelector');
+archivo.addEventListener("change",(e) => {
+    leerArchivo(archivo.files);
+});
+
+const  leerArchivo = (archives) => {
+
+    for(let archive of archives){
+        const reader = new FileReader();
+        reader.readAsDataURL(archive);
+        reader.addEventListener("load",(e)=>{
+            let newImage = `<img src='${e.currentTarget.result}'>`;
+            document.querySelector(".result").innerHTML += newImage;
+        });
+    }
+}
